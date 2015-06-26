72e7d027d644acfcff21c235d0f0261f72100b05
Rose Weixel <rose.weixel@gmail.com>
2015-06-25 21:02:06 -0400

    set up Mongoose and define schema

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/refactorme');
+
 var app = express();
 
 // view engine setup
--- models/post.js
+++ models/post.js
@@ -1,1 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+var PostSchema = new Schema({
+    name: String,
+    date: Date, 
+    creator: String,
+    content: String
+});
+
+module.exports = mongoose.model('Post', PostSchema);
