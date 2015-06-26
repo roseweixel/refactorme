25853a0c9e00b09375010d77c1237a994a30cf66
Rose Weixel <rose.weixel@gmail.com>
2015-06-25 21:05:50 -0400

    set up the router to work with api.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/refactorme');
 
 var app = express();
+var api = require('./routes/api');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
