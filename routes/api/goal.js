773c5da564b81782826912df6d770e039358afe5
Rose Weixel <rose.weixel@gmail.com>
2015-07-11 13:41:36 -0400

    finish all CRUD for goals

--- routes/api/goal.js
+++ routes/api/goal.js
@@ -20,9 +20,25 @@ module.exports.getAllGoals = function(req, res) {
     })
 };
 
-module.exports.getSingleGoal = function(req, res, id) {};
+module.exports.getSingleGoal = function(req, res, id) {
+    Goal.findById(id, function(err, goal) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({goal: goal})
+    });
+};
 
-module.exports.updateGoal = function(req, res, id) {};
+module.exports.updateGoal = function(req, res, id) {
+    // updates only the fields that are sent through in the form data
+    Goal.findByIdAndUpdate(id, {$set: req.body.goal}, function(err, goal) {
+        if (err) {
+            res.send(err);
+        }
+        // the previous version of the goal is being returned, though the goal is being updated
+        res.json({goal: goal});
+    });
+};
 
 module.exports.deleteGoal = function(req, res, id) {
     Goal.findByIdAndRemove(id, function(err) {
@@ -33,6 +49,7 @@ module.exports.deleteGoal = function(req, res, id) {
     });
 };
 
+// extra route I created for deleting all the goals at once
 module.exports.deleteGoals = function(req, res) {
     Goal.find(function(err, goals) {
         goals.forEach(function(goal) {
