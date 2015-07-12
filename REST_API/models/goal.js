e3162e2d21383155633e044b0ac067432e0b4129
Rose Weixel <rose.weixel@gmail.com>
2015-07-11 20:07:24 -0400

    show individual goal

--- REST_API/models/goal.js
+++ REST_API/models/goal.js
@@ -8,7 +8,7 @@ var GoalSchema = new Schema({
     creator: String,
     description: String
 });
-// {"goal":{"name":"Learn Node!","creator":"Rose","description":"This is going to be awesome!"}}
+// {"goal":{"name":"Learn Node!","creator":"Rose","description":"This is going to be awesome!","startDate":"07/07/2015","endDate":null}}
 
 module.exports = mongoose.model('Goal', GoalSchema);
 
