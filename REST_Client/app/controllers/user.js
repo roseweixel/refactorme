4f60297be4444d35efeace729d92834c8a47c996
Rose Weixel <rose.weixel@gmail.com>
2015-07-19 03:52:11 -0400

    create new goal associated with user

--- REST_Client/app/controllers/user.js
+++ REST_Client/app/controllers/user.js
@@ -6,20 +6,24 @@ export default Ember.ObjectController.extend({
   userID: function() {
     return this.get("model.id");
   }.property("model.id"),
+  user: function() {
+    return this.get("model");
+  }.property("model"),
 
   actions: {
     createGoal: function(){
       var name = this.get('newName');
       var description = this.get('newDescription');
-      var user = this.get('userID');
+      var userID = this.get('userID');
+      var startDate = new Date();
 
       var goal = this.store.createRecord('goal', {
         name: name,
         description: description,
-        startDate: startDate,
-        user: user
+        user: userID,
+        startDate: startDate
       });
-
+      this.get('user').get('goals').addObject(goal);
       goal.save();
     }
   }
