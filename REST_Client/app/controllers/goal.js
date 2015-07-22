848ff23ced96fc9ebecb9ce8e9f8a1afc57f00c4
Rose Weixel <rose.weixel@gmail.com>
2015-07-21 21:03:49 -0400

    started work on editing

---
+++ REST_Client/app/controllers/goal.js
@@ -0,0 +1,31 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+  editing: false,
+  actions: {
+    startEdit: function(){
+      this.set('editing', true);
+    },
+    saveGoal: function(){
+      var goal = this.get('model');
+      var name = this.get('model.name');
+      var description = this.get('model.description');
+
+      goal.set("name", name);
+      goal.set("description", description);
+
+      var controller = this;
+      goal.save()
+      .then(function(){
+        controller.set('editing', false);
+      });
+    },
+    deleteGoal: function(){
+      var goal = this.get('model');
+      var controller = this;
+      goal.destroyRecord().then(function(){
+        controller.transitionToRoute('goals');
+      });
+    }
+  }
+});
